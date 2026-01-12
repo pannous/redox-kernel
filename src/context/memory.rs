0e9fdaf4024358b18bb62c176eb55e2d7ae0bbc1
pannous <info@pannous.com>
2026-01-12 21:17:16 +0100

    chore: downgrade zeroed_phys_contiguous warning to trace
    Non-power-of-two allocations are handled gracefully by rounding up,
    so warn! level is unnecessarily noisy during normal driver operation.

--- src/context/memory.rs
+++ src/context/memory.rs
@@ -1229,7 +1229,7 @@ impl Grant {
         flusher: &mut Flusher,
     ) -> Result<Grant, Enomem> {
         if !span.count.is_power_of_two() {
-            warn!("Attempted non-power-of-two zeroed_phys_contiguous allocation, rounding up to next power of two.");
+            trace!("Attempted non-power-of-two zeroed_phys_contiguous allocation, rounding up to next power of two.");
         }
 
         let alloc_order = span.count.next_power_of_two().trailing_zeros();
